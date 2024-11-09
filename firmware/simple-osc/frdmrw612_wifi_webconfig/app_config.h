dabebc2dbafd23d7d3ed540a047896ef52d17b1d
aburt2 <32888184+aburt2@users.noreply.github.com>
2024-11-09 15:04:17 +1100

    fixed schematic and pcb to support in circuit programming

--- firmware/simple-osc/frdmrw612_wifi_webconfig/app_config.h
+++ firmware/simple-osc/frdmrw612_wifi_webconfig/app_config.h
@@ -5,7 +5,7 @@
  *  SPDX-License-Identifier: BSD-3-Clause
  */
 
-#define RW612
+#define RW610
 #define FRDMRW610
 #define WIFI_BT_USE_IMU_INTERFACE
 #define WIFI_BT_TX_PWR_LIMITS "wlan_txpwrlimit_cfg_WW_rw610.h"
